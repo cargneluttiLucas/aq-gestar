36ad287a7ab06f12fbd55978e3a465833de041c4
unknown <you@example.com>
2020-01-30 05:03:14 -0300

    feat: se agrega environment para tst

--- src/app/services/new-project.service.ts
+++ src/app/services/new-project.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-// import { environmentStaging } from 'src/environments/environment.staging';
-import { environmentProd } from 'src/environments/environment.prod';
+import { environmentStaging } from 'src/environments/environment.staging';
+// import { environmentProd } from 'src/environments/environment.prod';
 
 @Injectable()
 
@@ -19,32 +19,32 @@ export class NewProjectService {
 
   public getNewProyect(sessionId: string) {
     this.options.headers.SessionId = sessionId;
-    return this.http.get<any>(environmentProd.addresses.project.getNewProyect, this.options);
+    return this.http.get<any>(environmentStaging.addresses.project.getNewProyect, this.options);
   }
 
   public getOpenProyect(projectId: number, sessionId: string) {
     this.options.headers.SessionId = sessionId;
-    return this.http.get<any>(environmentProd.addresses.project.getOpenProyect + projectId, this.options);
+    return this.http.get<any>(environmentStaging.addresses.project.getOpenProyect + projectId, this.options);
   }
 
   putSaveProject(data: any, sessionId: string) {
     this.options.headers.SessionId = sessionId;
-    return this.http.post<any>(environmentProd.addresses.project.putSaveProject, data, this.options);
+    return this.http.post<any>(environmentStaging.addresses.project.putSaveProject, data, this.options);
   }
 
   putChangeProject(data: any, projectId: number, sessionId: string) {
     this.options.headers.SessionId = sessionId;
-    return this.http.put<any>(environmentProd.addresses.project.putChangeProject + projectId, data, this.options);
+    return this.http.put<any>(environmentStaging.addresses.project.putChangeProject + projectId, data, this.options);
   }
 
   findSponsor(filter, sessionId) {
     this.options.headers.SessionId = sessionId;
-    return this.http.post<any>(environmentProd.addresses.user.findUser, filter, this.options);
+    return this.http.post<any>(environmentStaging.addresses.user.findUser, filter, this.options);
   }
 
   findCliend(filter, sessionId) {
     this.options.headers.SessionId = sessionId;
-    return this.http.post<any>(environmentProd.addresses.contact.findCliend, filter, this.options);
+    return this.http.post<any>(environmentStaging.addresses.contact.findCliend, filter, this.options);
   }
 
 }
--- src/app/services/requirements.service.ts
+++ src/app/services/requirements.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-// import { environmentStaging } from 'src/environments/environment.staging';
-import { environmentProd } from 'src/environments/environment.prod';
+import { environmentStaging } from 'src/environments/environment.staging';
+// import { environmentProd } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -20,41 +20,41 @@ export class RequierementsService {
 
   public getNewSelects(sessionId: string) {
     this.options.headers.SessionId = sessionId;
-    return this.http.get<any>(environmentProd.addresses.requirement.getNewSelects, this.options);
+    return this.http.get<any>(environmentStaging.addresses.requirement.getNewSelects, this.options);
   }
 
   public getNewRequirements(sessionId: string) {
     this.options.headers.SessionId = sessionId;
-    return this.http.get<any>(environmentProd.addresses.requirement.getNewRequirements, this.options);
+    return this.http.get<any>(environmentStaging.addresses.requirement.getNewRequirements, this.options);
   }
 
   public getOpenByDocId(requirementId: number, sessionId: string) {
     this.options.headers.SessionId = sessionId;
-    return this.http.get<any>(environmentProd.addresses.requirement.getOpenByDocId + requirementId, this.options);
+    return this.http.get<any>(environmentStaging.addresses.requirement.getOpenByDocId + requirementId, this.options);
   }
 
   saveNewRequirement(data: any, sessionId) {
     this.options.headers.SessionId = sessionId;
-    return this.http.post<any>(environmentProd.addresses.requirement.saveNewRequirement, data, this.options);
+    return this.http.post<any>(environmentStaging.addresses.requirement.saveNewRequirement, data, this.options);
   }
 
   changeRequirementById(data: any, requirementId: number, sessionId) {
     this.options.headers.SessionId = sessionId;
-    return this.http.put<any>(environmentProd.addresses.requirement.changeRequirementById + requirementId, data, this.options);
+    return this.http.put<any>(environmentStaging.addresses.requirement.changeRequirementById + requirementId, data, this.options);
   }
 
   searchProject(filterData, sessionId) {
     this.options.headers.SessionId = sessionId;
-    return this.http.post<any>(environmentProd.addresses.project.searchProject, filterData, this.options);
+    return this.http.post<any>(environmentStaging.addresses.project.searchProject, filterData, this.options);
   }
 
   searchUsers(filterData, sessionId) {
     this.options.headers.SessionId = sessionId;
-    return this.http.post<any>(environmentProd.addresses.user.findUser, filterData, this.options);
+    return this.http.post<any>(environmentStaging.addresses.user.findUser, filterData, this.options);
   }
 
   searchActivities(filterData, sessionId) {
     this.options.headers.SessionId = sessionId;
-    return this.http.post<any>(environmentProd.addresses.activities.searchActivities, filterData, this.options);
+    return this.http.post<any>(environmentStaging.addresses.activities.searchActivities, filterData, this.options);
   }
 }
